d3a554895d145b34ff3e5b8277e21f75f51a3544
Luca Morelli <luca.morelli@wellnet.it>
2020-03-10 19:17:08 +0100

    App state and loading animation - add application action and reducer

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,14 +1,12 @@
 /**
  * Defines types for the available actions and store related stuff.
  */
-import {
-  DispatchProp as DispatchAPI,
-} from "react-redux";
+import { DispatchProp as DispatchAPI } from "react-redux";
 
 import { MessageActions } from "./message";
+import { ApplicationActions } from "./application";
 
-export type Action =
-  | MessageActions;
+export type Action = ApplicationActions | MessageActions;
 
 export type Dispatch = DispatchAPI<Action>;
 
