fa0bf8ada03815b2928e3eb0976912501952d415
Luca Morelli <luca.morelli@wellnet.it>
2020-03-11 15:02:05 +0100

    Theming - add header

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import Content from './Content';
+import React from "react";
+import Content from "./Content";
+
+function Header() {
+  return (
+    <header>
+      <div
+        style={{
+          margin: "1rem 0",
+          textAlign: "center",
+          borderBottom: "2px solid black"
+        }}
+      >
+        <h1>Beat the Nerd</h1>
+      </div>
+    </header>
+  );
+}
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Content />
     </div>
   );
